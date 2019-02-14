63befad9b47904d598bad37c69d684ccaa659a8b
renfei <renfei_hn@163.com>
2019-02-14 10:36:39 +0800

    调整到期日期至20191231

--- src/main/java/com/is/eus/service/support/BasicImplEntity.java
+++ src/main/java/com/is/eus/service/support/BasicImplEntity.java
@@ -7,7 +7,7 @@ public class BasicImplEntity {
     public void get() {
         try {
             String strDate = new SimpleDateFormat("yyyyMMdd").format(new Date()).toString().trim();
-            String strDate2 = "20190215";
+            String strDate2 = "20191231";
 
             Integer d1 = new Integer(strDate);
             Integer d2 = new Integer(strDate2);
